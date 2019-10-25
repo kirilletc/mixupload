c1138d7b0635e2512f6daf990214b233e3118f7a
Кирилл Волков <kvolkov@scanex.ru>
2019-10-25 16:55:01 +0300

    HTML MarkUp with non-adaptive v.1.0

---
+++ js/fullPageInit.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+         $('#fullpage').fullpage({
+         navigation: true,
+         navigationPosition: 'left',
+         // navigationTooltips: ['section1', 'section2','section3','section4',],
+         // showActiveTooltip: false,
+         slidesNavigation: true,
+         slidesNavPosition: 'bottom',
+         controlArrows:false,
+         });
+         });
